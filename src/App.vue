d28128082e1a47c9087a9a389141507ba21dac30
chi-chen-wei <ciennaccw@gmail.com>
2024-11-27 08:52:32 +0800

    add nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/projects">Projects</router-link> |
-    <router-link to="/events">Events</router-link> |
-    <router-link to="/contact">Contact</router-link>
-  </nav>
+  <NavigationBar />
   <router-view />
 </template>
+
+<script setup>
+import NavigationBar from "./components/NavigationBar.vue";
+</script>
 
 <style>
 #app {
@@ -16,18 +14,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav class="navbar">
+    <router-link to="/"
+      ><img class="logo" src="../assets/gdg_logo.svg"
+    /></router-link>
+    <div class="nav-links">
+      <router-link to="/about">About</router-link>
+      <router-link to="/projects">Projects</router-link>
+      <router-link to="/events">Events</router-link>
+      <router-link to="/contact">Contact</router-link>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+.navbar {
+  display: flex;
+  align-items: center;
+  padding: 30px;
+}
+.nav-links a {
+  margin-left: 50px;
+  font-size: large;
+  font-weight: bold;
+  color: #2c3e50;
+}
+.logo {
+  height: 50px;
+  width: auto;
+}
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
